c26cd319bb9975022b5e6b644e2d5bddcb2948d3
Akindipe Muheez <168924775+Akins-Coded@users.noreply.github.com>
2024-09-10 14:43:13 +0100

    Created a Basic dot py file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+addition = number1 + number2
+print("The Addition of", number1, "and", number2, "is",  addition)
+subtraction = number1 - number2
+print("The Subtraction of", number1, "and", number2, "is",  subtraction)
+multiplication = number1 * number2
+print("The Multiplication of", number1, "and", number2, "is",  multiplication)
