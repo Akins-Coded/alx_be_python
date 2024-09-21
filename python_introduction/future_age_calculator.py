5c2383ac44aa1fdc6c75de327ca3b598786a0012
Akindipe Muheez <168924775+Akins-Coded@users.noreply.github.com>
2024-09-21 03:06:11 +0100

    Started The control Flow projects

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,3 +1,13 @@
-user = int(input("How Old are you?", ))
-age = user + 27
-print(f"In 2050, you will be ", age, "years old")
+score = int(input("Enter the Student's Score: "))
+if score >= 90:
+  grade = "A"
+elif score >= 80:
+  grade = "B"
+elif score >= 70:
+  grade = "C"
+elif score >= 50:
+  grade = "D"
+else:
+  grade = "F"
+
+print("Your grade is:", grade)
