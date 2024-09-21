4335420ed109dd56ccb521ff417fa5a6293b09e8
Akindipe Muheez <168924775+Akins-Coded@users.noreply.github.com>
2024-09-21 04:40:31 +0100

    Fix match statement

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,19 +2,19 @@ num1 = float(input("Enter the first number: "))
 operator = (input("Choose the operation (+, -, *, /): "))
 num2 = float(input("Enter the second number: "))
 result = None
-if operator == "+":
-    result = num1 + num2
-elif operator == "-":
-    result = num1 - num2
-elif operator == "*":
-    result = num1 * num2
-elif operator == "/":
-    if num2 != 0:
-        result = num1 / num2
-    else:
-        print("Cannot divide by zero.")
-
-else:
-    print("Invalid operation!")
+match operator:
+    case "+":
+        result = num1 + num2
+    case "-":
+        result = num1 - num2
+    case "*":
+        result = num1 * num2
+    case "/":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            print("Cannot divide by zero.")
+    case _:
+        print("Invalid operation!")
 if result is not None:
     print(f"The result is {result} ")
