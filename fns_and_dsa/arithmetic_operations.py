e675328a23f655d792f2ae6f5de2d102cb17eded
Akindipe Muheez <168924775+Akins-Coded@users.noreply.github.com>
2024-09-29 20:32:10 +0100

    Checker

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,6 +10,7 @@ def perform_operation(num1, num2, operation):
     elif operation == 'divide':
         if num2 == 0:
             return "Error: Division by zero is not allowed. "
-        return num1 / num2
+        else:
+            return num1 / num2
     else:
         return "Error: Invalid Operation. Please use 'add', 'subtract', 'multiply', or 'divide'."
