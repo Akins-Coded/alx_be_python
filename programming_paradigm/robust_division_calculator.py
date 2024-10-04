6d36c01cecc9c1121082b0bc89960b1d1c90ad86
Akindipe Muheez <168924775+Akins-Coded@users.noreply.github.com>
2024-10-04 09:43:37 +0100

    Checker

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,13 +6,10 @@ def safe_divide(numerator, denominator):
         denom = float(denominator)
         result = num / denom
         
-        return f"The result is {result}: "
-
+        return f"The result of the division is {result}"
+    
     except ZeroDivisionError:
-        return "Error: Cannot divide by Zero"
-
+        return "Error: Cannot divide by zero."
     except ValueError:
-        return "Error: Please Enter Numeric Value Only"
-
-
+        return "Error: Please enter numeric values only."
     
