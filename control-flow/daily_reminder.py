d847f35b39c09086c2e7cd784cc1d7300339cb5a
Akindipe Muheez <akindipemuheez@gmail.com>
2024-10-29 08:48:18 +0100

    Checker

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,7 @@
-task = input("Describe your Task: ")
+task = input("Enter your Task: ")
+time_bound = input("Is it Task time-bound?  (yes or no) : ").lower()
 priority = input("What is the Priority Level for this Task (high, medium, low) ? ").lower()
-time_bound = input("IS The Task Time Bound  (yes or no) : ").lower()
+
 
 
 match priority:
